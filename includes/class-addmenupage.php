b054a1896beb84fd7a2cdd7ac61ea11a26078634
Harit Panchal <panchalharit007@gmail.com>
2022-12-12 14:35:14 +0530

    email template module completed

--- includes/class-addmenupage.php
+++ includes/class-addmenupage.php
@@ -22,8 +22,7 @@ class AddMenuPage {
 	 */
 	public function __construct() {
 		add_action( 'admin_menu', array( $this, 'email_crons_register_admin' ) );
-		// add_action( 'admin_notices', array( $this, 'sample_admin_notice__success' ) );
-		// add_action( 'admin_notices', array( $this, 'sample_admin_notice__error' ) );
+		add_action( 'admin_post_nds_form_response', array( $this, 'the_form_response' ) );
 	}
 
 	/**
@@ -44,6 +43,16 @@ class AddMenuPage {
 
 		$GLOBALS['email-crons-users'] = add_submenu_page(
 			'email-crons.php',
+			'Email Test',
+			'Email Test',
+			'manage_options',
+			'email-crons.php&tab=email-test',
+			array( $this, 'email_crons_template' ),
+			'dashicons-share-alt2',
+		);
+
+		$GLOBALS['email-crons-users'] = add_submenu_page(
+			'email-crons.php',
 			'Users',
 			'Users',
 			'manage_options',
@@ -51,6 +60,7 @@ class AddMenuPage {
 			array( $this, 'email_crons_template' ),
 			'dashicons-share-alt2',
 		);
+
 	}
 
 	/**
@@ -65,7 +75,7 @@ class AddMenuPage {
 		}
 
 		$default_tab = null;
-		$tab         = isset( $_GET['tab'] ) ? $_GET['tab'] : $default_tab;
+		$tab         = isset( $_GET['tab'] ) ? $_GET['tab'] : $default_tab; //phpcs:ignore
 		$screen      = get_current_screen();
 
 		$active_class = '';
@@ -76,6 +86,9 @@ class AddMenuPage {
 		if ( 'users' === $tab ) {
 			$users = 'nav-tab-active';
 		}
+		if ( 'email-test' === $tab ) {
+			$email_test = 'nav-tab-active';
+		}
 
 		?>
 		<div class="wrap">
@@ -83,6 +96,7 @@ class AddMenuPage {
 
 			<nav class="nav-tab-wrapper">
 				<a href="?page=<?php echo esc_attr( $screen->parent_file ); ?>" class="nav-tab <?php echo esc_attr( $default ); ?>">Email Template</a>
+				<a href="?page=<?php echo esc_attr( $screen->parent_file ); ?>&tab=email-test" class="nav-tab <?php echo esc_attr( $email_test ); ?>">Email Test</a>
 				<a href="?page=<?php echo esc_attr( $screen->parent_file ); ?>&tab=users" class="nav-tab <?php echo esc_attr( $users ); ?>">Users</a>
 			</nav>
 
@@ -91,6 +105,9 @@ class AddMenuPage {
 				switch ( $tab ) :
 					case 'users':
 						echo 'users';
+						break;
+					case 'email-test':
+						echo 'email-test';
 						break;
 					default:
 						$this->email_crons_email_template_tab_callback();
@@ -110,9 +127,26 @@ class AddMenuPage {
 	 */
 	public function email_crons_email_template_tab_callback() {
 		?>
-			<h3>Write Your E-mail Template</h3>
+			<!-- <h3>Write Your E-mail Template</h3> -->
 		<?php
-		$content            = '';
+
+		if ( 'updated' === get_transient( 'updated_option' ) ) {
+			?>
+				<div class="notice notice-success is-dismissible">
+					<p><?php echo esc_attr( 'Template saved!' ); ?></p>
+				</div>
+			<?php
+			delete_transient( 'updated_option' );
+		}
+		if ( get_transient( 'update_fail' ) ) {
+			?>
+			<div class="notice notice-error">
+				<p><?php echo esc_attr( 'An error has occurred' ); ?></p>
+			</div>
+			<?php
+			delete_transient( 'update_fail' );
+		}
+		$content            = get_option( 'email_crons_email_template_editor_name' );
 		$custom_editor_id   = 'email_crons_email_template_editor';
 		$custom_editor_name = 'email_crons_email_template_editor_name';
 		$args               = array(
@@ -121,27 +155,40 @@ class AddMenuPage {
 			'textarea_rows' => get_option( 'default_post_edit_rows', 10 ),
 			'quicktags'     => true,
 		);
-		wp_editor( $content, $custom_editor_id, $args );
+		$nds_add_meta_nonce = wp_create_nonce( 'nds_add_user_meta_form_nonce' );
 
-		submit_button( __( 'Save Template', 'email-crons' ) );
-
-	}
-
-	public function sample_admin_notice__success() {
 		?>
-		<div class="notice notice-success is-dismissible">
-			<p><?php _e( 'Done!', 'sample-text-domain' ); ?></p>
-		</div>
+		<form action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="post" id="nds_add_user_meta_form"> 
+			<input type="hidden" name="action" value="nds_form_response">
+			<input type="hidden" name="nds_add_user_meta_nonce" value="<?php echo esc_attr( $nds_add_meta_nonce ); ?>" />			
+			<?php
+				wp_editor( $content, $custom_editor_id, $args );
+				submit_button( __( 'Save Template', 'email-crons' ) );
+			?>
+		</form>
 		<?php
 	}
 
-	public function sample_admin_notice__error() {
-		$class   = 'notice notice-error';
-		$message = __( 'Irks! An error has occurred.', 'sample-text-domain' );
 
-		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+	/**
+	 * Function the_form_response callback.
+	 *
+	 * @since 1.0.0
+	 */
+	public function the_form_response() {
+		if ( isset( $_POST['nds_add_user_meta_nonce'] ) && wp_verify_nonce( wp_unslash( $_POST['nds_add_user_meta_nonce'] ), 'nds_add_user_meta_form_nonce' ) ) { //phpcs:ignore
+			$content = isset( $_POST['email_crons_email_template_editor_name'] ) ? wp_kses_post( wp_unslash( $_POST['email_crons_email_template_editor_name'] ) ) : '';
+			update_option( 'email_crons_email_template_editor_name', $content );
+			set_transient( 'updated_option', 'updated', 10 );
+			wp_safe_redirect( admin_url( 'admin.php?page=email-crons.php' ) );
+			die();
+		} else {
+			set_transient( 'update_fail', 'fail', 10 );
+			wp_safe_redirect( admin_url( 'admin.php?page=email-crons.php' ) );
+			die();
+			// wp_die( 'Invalid nonce specified' );
+		}
 	}
-
 }
 
 $add_menu_page = new AddMenuPage();
