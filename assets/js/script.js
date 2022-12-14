d90c6d7fdcd6fc52c27f37ca9dfbae7c5b2931c8
Harit Panchal <panchalharit007@gmail.com>
2022-12-15 00:27:57 +0530

    users based on roles functionality added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 (function ($) {
   jQuery(document).ready(function () {
+    $("#email_crons_users").select2();
+    $("#email_crons_roles").select2();
+
     $("#email_crons_sent_test_email").click(function () {
       var test_email = $("#email_crons_test_email").val();
       jQuery.ajax({
@@ -22,14 +25,39 @@
       });
     });
 
-    $("#email_crons_users").select2();
-    $("#email_crons_roles").select2();
+    $("#email_crons_roles").on("change", function () {
+      $("#email_crons_users").val(null).trigger("change");
+      var roles = $("#email_crons_roles").select2("val");
+      console.log(roles);
+      jQuery.ajax({
+        url: localize_variable.ajax_url,
+        type: "POST",
+        data: {
+          action: "email_crons_change_users",
+          roles: roles,
+        },
+        success: function (response) {
+          console.log(response);
+          if (response !== null) {
+            $('#email_crons_users').empty().append(response)
+          }
+          // console.log(response);
+          // jQuery(".test_message p").text(response.data.message);
+          // if (response.success === true) {
+          //   jQuery(".test_message").addClass("notice-success");
+          // } else {
+          //   jQuery(".test_message").addClass("notice-error");
+          // }
+        },
+      });
+    });
 
     $("#email_crons_select_all").click(function () {
       if ($("#email_crons_select_all").is(":checked")) {
         $("#email_crons_users > option").prop("selected", "selected");
         $("#email_crons_users").trigger("change");
       } else {
+        console.log("here");
         $("#email_crons_users > option").removeAttr("selected");
         $("#email_crons_users").trigger("change");
       }
--- includes/tabs/class-usersselection.php
+++ includes/tabs/class-usersselection.php
@@ -23,6 +23,7 @@ class UsersSelection {
 	 */
 	public function __construct() {
 		add_action( 'admin_post_email_crons_save_users', array( $this, 'email_crons_save_users_callback' ) );
+		add_action( 'wp_ajax_email_crons_change_users', array( $this, 'email_crons_change_users_callback' ) );
 	}
 
 	/**
@@ -30,10 +31,11 @@ class UsersSelection {
 	 */
 	public function users_selection_callback() {
 		global $wp_roles;
-		$user_roles              = $wp_roles->get_names();
+		$user_roles              = array_keys( $wp_roles->roles );
 		$email_crons_users_nonce = wp_create_nonce( 'email_crons_save_users_nonce_value' );
 		$all_users               = get_users();
 		$selected_users          = get_option( 'email_crons_users_chunk', true ) ? get_option( 'email_crons_users_chunk', true ) : '';
+		$selected_roles          = get_option( 'email_crons_roles_chunk', true ) ? get_option( 'email_crons_roles_chunk', true ) : '';
 		$selected_users_count    = is_array( $selected_users ) ? count( $selected_users ) : '';
 		$all_users_id            = array_column( $all_users, 'user_login' );
 		$select_all_checked      = count( $all_users_id ) === $selected_users_count ? 'checked' : '';
@@ -42,57 +44,59 @@ class UsersSelection {
 		<p>Select the users to whom you want to send your mail template.</p>
 			<div>
 				<label>Select role(s):</label></br>
-				<select id="email_crons_roles" name="email_crons_roles[]" multiple="multiple" style="width:300px">
-					<?php
-					foreach ( $user_roles as $user_role ) {
-						?>
-								<option value="<?php echo esc_attr( $user_role ); ?>">
-							<?php
-							echo esc_attr( $user_role ) . '(' . esc_attr( $user_role ) . ')';
-							?>
-									</option>
-						<?php
-					}
-					?>
-				</select>
 			</div>
 			<div>
-				<br/>
 				<form action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="post" id="email_crons_save_users_form"> 
 					<table>
 						<tbody>
 							<tr>
+								<select id="email_crons_roles" name="email_crons_roles[]" multiple="multiple" style="width:300px">
+								<?php
+								foreach ( $user_roles as $user_role ) {
+									$selected_roles_label = ( '' !== $selected_roles ) ? ( in_array( $user_role, $selected_roles, true ) ? 'selected' : '' ) : '';
+									?>
+											<option value="<?php echo esc_attr( $user_role ); ?>" <?php echo esc_attr( $selected_roles_label ); ?>>
+											<?php
+											echo esc_attr( $user_role ) . '(' . esc_attr( ucfirst( $user_role ) ) . ')';
+											?>
+											</option>
+										<?php
+								}
+								?>
+								</select>
+							</tr>
+							<tr>
 								<input id="email_crons_select_all" type="checkbox" <?php echo esc_attr( $select_all_checked ); ?>>Select All
 								<td>
 									<select id="email_crons_users" name="email_crons_users[]" multiple="multiple" style="width:300px">
-										<?php
-										foreach ( $all_users as $user ) {
-											$user_id    = $user->data->ID;
-											$user_name  = $user->data->display_name;
-											$user_email = $user->data->user_email;
-											$selected   = ( '' !== $selected_users ) ? ( in_array( $user_id, $selected_users, true ) ? 'selected' : '' ) : '';
+									<?php
+									foreach ( $all_users as $user ) {
+										$user_id              = $user->data->ID;
+										$user_name            = $user->data->display_name;
+										$user_email           = $user->data->user_email;
+										$selected_users_label = ( '' !== $selected_users ) ? ( in_array( $user_id, $selected_users, true ) ? 'selected' : '' ) : '';
+										?>
+												<option value="<?php echo esc_attr( $user_id ); ?>" <?php echo esc_attr( $selected_users_label ); ?>>
+											<?php
+											echo esc_attr( $user_name ) . '(' . esc_attr( $user_email ) . ')';
 											?>
-												<option value="<?php echo esc_attr( $user_id ); ?>" <?php echo esc_attr( $selected ); ?>>
-												<?php
-												echo esc_attr( $user_name ) . '(' . esc_attr( $user_email ) . ')';
-												?>
 												</option>
 											<?php
-										}
-										?>
+									}
+									?>
 									</select>
 								</td>
 							</tr>
 						</tbody>
 					</table>
-					<?php
+						<?php
 						submit_button( __( 'Save Users', 'email-crons' ) );
-					?>
+						?>
 					<input type="hidden" name="action" value="email_crons_save_users">
 					<input type="hidden" name="email_crons_users_nonce" value="<?php echo esc_attr( $email_crons_users_nonce ); ?>" />			
 				</form>
 			</div>
-		<?php
+			<?php
 	}
 
 	/**
@@ -101,12 +105,29 @@ class UsersSelection {
 	public function email_crons_save_users_callback() {
 		if ( isset( $_POST['email_crons_users_nonce'] ) && wp_verify_nonce( wp_unslash( $_POST['email_crons_users_nonce'] ), 'email_crons_save_users_nonce_value' ) ) { //phpcs:ignore
 			$users = isset( $_POST['email_crons_users'] ) ? $_POST['email_crons_users'] : ''; //phpcs:ignore
-
-			// if ( is_array( $users ) ) {
-				update_option( 'email_crons_users_chunk', $users );
-			// }
+			$roles = isset( $_POST['email_crons_roles'] ) ? $_POST['email_crons_roles'] : ''; //phpcs:ignore
+			update_option( 'email_crons_users_chunk', $users );
+			update_option( 'email_crons_roles_chunk', $roles );
 		}
 		wp_safe_redirect( admin_url( 'admin.php?page=email-crons.php&tab=users' ) );
+		exit;
+	}
+
+	/**
+	 * Email crons change users callback function
+	 */
+	public function email_crons_change_users_callback() {
+		$selected_roles = isset( $_POST['roles'] ) ? $_POST['roles'] : ''; //phpcs:ignore
+		$new_users      = get_users( array( 'role__in' => $selected_roles ) );
+		foreach ( $new_users as $user ) {
+			$user_id    = $user->data->ID;
+			$user_name  = $user->data->display_name;
+			$user_email = $user->data->user_email;
+
+			$options[] = '<option value="' . esc_attr( $user_id ) . '"' . esc_attr( $selected_users_label ) . '>' . esc_attr( $user_name ) . '(' . esc_attr( $user_email ) . ') </option >';
+		}
+
+		wp_send_json( $options );
 		exit;
 	}
 }
